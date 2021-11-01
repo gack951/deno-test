864261d1cadc8c0f947219a72e7db8bfa605b7db
gack951 <gack951@gmail.com>
2021-11-01 19:11:08 +0900

    routing with variables

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,11 @@ app.get("/", function (req, res) {
     console.log(req.query);
     res.send("Hello World");
 });
+app.get("/api/:id", function (req, res) {
+    console.log("api", req.params.id);
+    res.send("Hello World");
+});
 
 app.listen(parseInt(env.port), () => {
-    console.log("server has started on http://localhost:" + env.port);
+    console.log("listening on http://localhost:" + env.port);
 });
